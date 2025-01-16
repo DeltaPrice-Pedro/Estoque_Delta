71b49e9c2ce9159adc76fd0ba368ddca7973e6c5
DeltaPrice-Pedro <deltapricepedro@gmail.com>
2025-01-16 11:33:38 -0300

    form and AuthStateChanges

--- lib/login/login.dart
+++ lib/login/login.dart
@@ -1,91 +1,113 @@
-import 'package:estoque_delta/login/input_login.dart';
-import 'package:estoque_delta/models/warning_dialog.dart';
+// import 'package:estoque_delta/login/input_login.dart';
+import 'package:estoque_delta/login/warning_dialog.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+
 final _firebase = FirebaseAuth.instance;
+final _formKey = GlobalKey<FormState>();
+var _phoneUser = '';
 
 class Login extends StatelessWidget {
-  const Login(this.method, {super.key});
-  final void Function() method;
+  const Login({super.key});
 
+  void _submit() async {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      _sendToFirebase();
+    } else {
+      return;
+    }
+  }
+
+  _sendToFirebase() {
+    try {
+      final userCredentials = _firebase.signInWithPhoneNumber(_phoneUser);
+      print(userCredentials);
+    } on FirebaseAuthException catch (error) {
+      WarningDialog(error.message ?? 'Dados inválidos');
+    }
+  }
 
   @override
   Widget build(context) {
-    String nameUser = '';
-    String psswrdUser = '';
-
-    void validAcess() {
-      if (nameUser == '' && psswrdUser == '') {
-        showDialog(
-            context: context,
-            builder: (context) => WarningDialog('Motivo do erro'));
-      } else {
-        method();
-      }
-    }
-
-    return Column(
-      children: [
-        Container(
-          decoration: BoxDecoration(boxShadow: [
-            BoxShadow(
-              color: const Color.fromARGB(124, 255, 255, 255),
-              spreadRadius: 10,
-              blurRadius: 30,
-              offset: Offset(0, 3), // changes position of shadow
-            ),
-          ], color: Colors.white, borderRadius: BorderRadius.circular(10.0)),
-          clipBehavior: Clip.antiAlias,
-          padding: EdgeInsets.all(20.0),
-          margin: EdgeInsets.only(top: 80.0),
+    return Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text(
-                'Bem vindo(a) a lojinha',
-                textAlign: TextAlign.center,
-                style: GoogleFonts.poppins(fontSize: 25.0),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Image.asset(
-                  'assets/images/deltaprice_hori.png',
-                  width: 350,
+              Container(
+                decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        color: const Color.fromARGB(124, 255, 255, 255),
+                        spreadRadius: 10,
+                        blurRadius: 30,
+                        offset: Offset(0, 3), // changes position of shadow
+                      ),
+                    ],
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10.0)),
+                clipBehavior: Clip.antiAlias,
+                padding: EdgeInsets.all(20.0),
+                margin: EdgeInsets.only(top: 80.0),
+                child: Column(
+                  children: [
+                    Text(
+                      'Bem vindo(a) a lojinha',
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.poppins(fontSize: 25.0),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Image.asset(
+                        'assets/images/deltaprice_hori.png',
+                        width: 350,
+                      ),
+                    ),
+                  ],
                 ),
               ),
+              Container(
+                padding: EdgeInsets.all(20.0),
+                alignment: Alignment.centerLeft,
+                margin: EdgeInsets.only(top: 70.0),
+                color: Colors.white,
+                child: Form(
+                  child: TextFormField(
+                      decoration: const InputDecoration(
+                          labelText: 'Número de telefone'),
+                      keyboardType: TextInputType.phone,
+                      validator: (value) {
+                        if (value == null ||
+                            value.trim().isEmpty ||
+                            value.trim().length < 9) {
+                          return 'Favor inserir número de telefone válido';
+                        }
+                        return null;
+                      },
+                      onSaved: (newValue) {
+                        _phoneUser = newValue!;
+                      }),
+                ),
+              ),
+              //InputLogin(),
+              Padding(
+                padding: const EdgeInsets.only(top: 60.0),
+                child: ElevatedButton(
+                  onPressed: _submit,
+                  // style: ButtonStyle(backgroundColor: Colors.white),
+                  child: Text(
+                    "Enviar",
+                    style: GoogleFonts.poppins(fontSize: 15),
+                  ),
+                ),
+              )
             ],
           ),
         ),
-        Container(
-          padding: EdgeInsets.all(20.0),
-          alignment: Alignment.centerLeft,
-          margin: EdgeInsets.only(top: 70.0),
-          color: Colors.white,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              InputLogin('Nome completo', (String value) {
-                nameUser = value;
-              }),
-              InputLogin('Senha', (String value) {
-                psswrdUser = value;
-              }),
-            ],
-          ),
-        ),
-        //InputLogin(),
-        Padding(
-          padding: const EdgeInsets.only(top: 60.0),
-          child: ElevatedButton(
-            onPressed: validAcess,
-            // style: ButtonStyle(backgroundColor: Colors.white),
-            child: Text(
-              "Enviar",
-              style: GoogleFonts.poppins(fontSize: 15),
-            ),
-          ),
-        )
-      ],
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:estoque_delta/login/login.dart';
 import 'package:estoque_delta/menu/menu.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:estoque_delta/splash_screen.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -12,47 +14,28 @@ void main() async {
   runApp(const App());
 }
 
-class App extends StatefulWidget {
+class App extends StatelessWidget {
   const App({super.key});
-
-  @override
-  State<App> createState() {
-    return _App();
-  }
-}
-
-class _App extends State<App> {
-  Widget? activePage;
-
-  @override
-  void initState() {
-    activePage = Login(switchPage);
-    super.initState();
-  }
-
-  void switchPage() {
-    setState(() {
-      activePage = const Menu();
-    });
-  }
 
   @override
   Widget build(context) {
     return MaterialApp(
         title: 'Estoque Deltaprice',
-        theme: ThemeData(useMaterial3: true),
-        home: Scaffold(
-          body: Container(
-            decoration: BoxDecoration(
-                gradient: LinearGradient(colors: [
-              const Color.fromARGB(255, 26, 131, 250),
-              const Color.fromARGB(255, 74, 158, 254),
-            ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
-            // alignment: Alignment.topCenter,
-            child: Center(
-              child: activePage,
-            ),
-          ),
-        ));
+        theme: ThemeData(useMaterial3: true).copyWith(
+          colorScheme: ColorScheme.fromSeed(
+              seedColor: const Color.fromARGB(255, 26, 131, 250)),
+        ),
+        home: StreamBuilder(
+            stream: FirebaseAuth.instance.authStateChanges(),
+            builder: (cntx, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const SplashScreen();
+              }
+
+              if (snapshot.hasData) {
+                return const Menu();
+              }
+              return Login();
+            }));
   }
 }
